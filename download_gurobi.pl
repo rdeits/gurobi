c3cf641518d9ec341f99f74c7a7842cb63464ec3
Russ Tedrake <russt@mit.edu>
2015-03-21 22:06:21 -0400

    updates gurobi scripts to new website and to v 6.0.2

--- download_gurobi.pl
+++ download_gurobi.pl
@@ -5,6 +5,7 @@ use HTML::Form;
 use Term::ReadKey;
 use Config;
 
+local $| = 1;  # autoflush stdout
 my $filename;
 
 if ( $ENV{'GUROBI_DISTRO'} ) {
@@ -24,45 +25,47 @@ if ( $ENV{'GUROBI_DISTRO'} ) {
     chomp(my $password = <>);
     ReadMode('restore');
 
-    $response = $mech->get( "http://www.gurobi.com/account" );
+    $response = $mech->get( "http://www.gurobi.com/login" );
     @forms = HTML::Form->parse( $response );
-    
+
     $form = shift(@forms);  # search form
     $form = shift(@forms);  # login form
-    
-    $form->value('email',$email); 
+
+    $form->value('email',$email);
     $form->value('password',$password);
-    
+
     $response = $mech->request( $form->click );
-    
-    $response = $mech->get( "http://www.gurobi.com/download/gurobi-optimizer" );
-    
-    @forms = HTML::Form->parse( $response, "http://www.gurobi.com" );
-    
+
+    $response = $mech->get( "http://user.gurobi.com/download/gurobi-optimizer" );
+
+    @forms = HTML::Form->parse( $response, "http://user.gurobi.com" );
+
     my $numforms = scalar @forms;
     if ( $numforms > 2 ) { # login failed page has two forms
-      last; 
+      last;
     }
 
     print "Login failed.  Please try again.\n";
-  } 
+  }
 
   $form = shift(@forms);  # search form
   $form = shift(@forms);  # login form
   $form = shift(@forms);  # download form
-  
-  if ($^O eq 'darwin') {
-      $filename = 'gurobi5.6.2_mac64.pkg'; 
-  } elsif ($^O eq 'linux') {
-      $filename = 'gurobi5.6.2_linux64.tar.gz'; 
-  }
-  
-  $form->value('filename','5.6.2/'.$filename);
 
-  print "\n Downloading $filename ... ";
+  if ($^O eq 'darwin') {
+      $filename = 'gurobi6.0.2_mac64.pkg';
+      $postfix = '/Mac OS';
+  } elsif ($^O eq 'linux') {
+      $filename = 'gurobi6.0.2_linux64.tar.gz';
+      $postfix = '/Linux 64';
+  }
+
+  $form->value('filename','6.0.2/'.$filename);#.$postfix);
+
+  print "\nDownloading $filename ... ";
   $response = $mech->request( $form->click , $filename);
-#print $response->content();
   print "done\n";
+  print $response->content();
 
   chomp($cwd = `pwd`);
   $filename = "$cwd/$filename"
@@ -72,10 +75,10 @@ if ($^O eq 'darwin') {
   system("mkdir","tmp");
   chdir("tmp");
   system("xar","-xf","$filename");
-  chdir("gurobi562mac64tar.pkg");
+  chdir("gurobi602mac64tar.pkg");
   system("tar","-xvf","Payload");
-  system("tar","-xvf","gurobi5.6.2_mac64.tar.gz");
-  system("mv","gurobi562","../../");
+  system("tar","-xvf","gurobi6.0.2_mac64.tar.gz");
+  system("mv","gurobi602","../../");
   chdir("../..");
   system("rm","-rf","tmp");
 #  system("rm","-rf",$filename);
@@ -83,4 +86,3 @@ if ($^O eq 'darwin') {
   system("tar","-xvf",$filename);
 #  system("rm","-rf",$filename);
 }
-
